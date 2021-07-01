12a9127cb2a29157a01a105a23783268a814530e
Ahmed Hussein <ahmedhussein@Ahmeds-MacBook-Pro.local>
2021-07-01 16:01:54 -0600

    first

---
+++ intro.js
@@ -0,0 +1,12 @@
+const amaunt = 11; 
+
+if(amaunt < 10){
+    console.log("The amount less than 10"); 
+} else {
+    console.log("this amount is larger than 10")
+}
+
+
+console.log("this is my first node.js app")
+
+
